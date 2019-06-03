6b4b511fca9d79560cece6a75876df9c4f09fab4
mnphmnmn <nam_nam8@hotmail.com>
2019-06-04 04:51:45 +0700

    add wiki link

--- src/components/WordCloud.js
+++ src/components/WordCloud.js
@@ -43,6 +43,34 @@ class WordCloud extends Component {
             fontSizes: [10, 60],
             spiral: 'archimedean',
           }}
+          callbacks={{
+            getWordTooltip: ({ text, value }) => {
+              // axios
+              //   .get(
+              //     `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${text}&limit=1&namespace=0&format=json`,
+              //   )
+              //   .then(res => {
+              //     //0: "ideas"
+              //     // 1: ["Idea"]
+              //     // 2: ["In philosophy, ideas are usually taken as mental r…ct concepts that do not present as mental images."]
+              //     // 3: ["https://en.wikipedia.org/wiki/Idea"]
+              //     // console.log(res.data[2][0]);
+              //   });
+              return text;
+            },
+            onWordClick: ({ text }) => {
+              axios
+                .get(
+                  `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${text}&limit=1&namespace=0&format=json`,
+                )
+                .then(res => {
+                  if (res.data[3].length > 0) {
+                    window.open(res.data[3][0]);
+                  }
+                  // console.log(res.data[2][0]);
+                });
+            },
+          }}
           //   minSize={[400, 400]}
           //   size={[100, 100]}
         />
